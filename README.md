15bba98f00c87c306495ce862e9cfe637d086c6a
Kayleigh Page <k@abs.gd>
2025-04-17 13:29:31 +0200

    Basic setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-My personal website with a blog, porfolio and newsletter form.  
+My personal website with a blog, portfolio and newsletter form.  
 The backend code for all my personal websites and blogs can be found here: [https://github.com/kayleigh-page/blogger](https://github.com/kayleigh-page/blogger) .  
 The live website can be visited here: [https://kayleigh.page](https://kayleigh.page) .
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,63 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from "next-pwa";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const withPWA = nextPWA({
+  dest: "public",
+  disable: isDev, // Disable PWA in development
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/, /dynamic-css-manifest\.json$/],
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "orcid.org",
+        pathname: "/sites/default/files/images/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5001",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.blogger.abs.gd",
+        port: "443",
+        pathname: "/**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
+});
 
 export default nextConfig;
